96505e3b346347d22c378fa25b9c54578118611d
AntarticPenguin <kes99709@gmail.com>
2019-04-15 17:32:06 +0900

    1. 타일에디터 작업 - 휠로 카메라 축소/확대 - 타일 종류 그리드 스크롤 뷰로 띄우기 - 타일 클릭 후, 그리드에 설치   아직 지우는 기능 없음   덮어씌우기 가능
    - 작업물 csv로 파일 저장 / 열기

--- Assets/01.Scripts/TileMapEditor/GridTile.cs
+++ Assets/01.Scripts/TileMapEditor/GridTile.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class GridTile : MonoBehaviour
 {
-	static int _globalSortingOrder = 9999;
-	static int _MaxSortingOrder = 10000;
-	int _sortingOrder;
-
 	public PolygonCollider2D _collider;
 	Material _whiteMat = null;
 	Material _redMat = null;
 	public List<LineRenderer> _lineRenderers = new List<LineRenderer>();
+
+	int _sortingOrder;
 
 	private void Awake()
 	{
 		_collider = gameObject.AddComponent<PolygonCollider2D>();
 		_whiteMat = Resources.Load<Material>("Materials/whiteLineMat");
 		_redMat = Resources.Load<Material>("Materials/redLineMat");
+
+		_spriteName = "none";
 	}
 
 	// Start is called before the first frame update
 	void Start()
     {
-
+		
 	}
 
     // Update is called once per frame
@@ -41,26 +41,22 @@ public class GridTile : MonoBehaviour
 			li.transform.SetParent(transform);
 			li.material = _whiteMat;
 			li.sortingLayerID = SortingLayer.NameToID("ON_GROUND");
-			li.sortingOrder = _globalSortingOrder;
 			li.startWidth = 0.02f;
 			li.endWidth = 0.02f;
-			_sortingOrder = _globalSortingOrder;
 
 			_lineRenderers.Add(li);
 		}
-		DrawLine(_lineRenderers[0], new Vector3(transform.position.x, transform.position.y, 0),
-			new Vector3(transform.position.x + width / 2, transform.position.y + height / 2));
+		DrawLine(_lineRenderers[0], new Vector3(transform.position.x, transform.position.y, -1),
+			new Vector3(transform.position.x + width / 2, transform.position.y + height / 2, -1));
 
-		DrawLine(_lineRenderers[1], new Vector3(transform.position.x + width / 2, transform.position.y + height / 2),
-			new Vector3(transform.position.x, transform.position.y + height, 0));
+		DrawLine(_lineRenderers[1], new Vector3(transform.position.x + width / 2, transform.position.y + height / 2, -1),
+			new Vector3(transform.position.x, transform.position.y + height, -1));
 
-		DrawLine(_lineRenderers[2], new Vector3(transform.position.x, transform.position.y + height),
-			new Vector3(transform.position.x - width / 2, transform.position.y + height / 2, 0));
+		DrawLine(_lineRenderers[2], new Vector3(transform.position.x, transform.position.y + height, -1),
+			new Vector3(transform.position.x - width / 2, transform.position.y + height / 2, -1));
 
-		DrawLine(_lineRenderers[3], new Vector3(transform.position.x - width / 2, transform.position.y + height / 2, 0),
-			new Vector3(transform.position.x, transform.position.y, 0));
-
-		--_globalSortingOrder;
+		DrawLine(_lineRenderers[3], new Vector3(transform.position.x - width / 2, transform.position.y + height / 2, -1),
+			new Vector3(transform.position.x, transform.position.y, -1));
 	}
 
 	void DrawLine(LineRenderer li, Vector3 start, Vector3 end)
@@ -87,7 +83,7 @@ public class GridTile : MonoBehaviour
 			_lineRenderers[i].startWidth = 0.04f;
 			_lineRenderers[i].endWidth = 0.04f;
 			_lineRenderers[i].material = _redMat;
-			_lineRenderers[i].sortingOrder = _MaxSortingOrder;
+			_lineRenderers[i].sortingOrder = 9999;
 		}
 	}
 
@@ -98,7 +94,7 @@ public class GridTile : MonoBehaviour
 			_lineRenderers[i].startWidth = 0.02f;
 			_lineRenderers[i].endWidth = 0.02f;
 			_lineRenderers[i].material = _whiteMat;
-			_lineRenderers[i].sortingOrder = _sortingOrder;
+			_lineRenderers[i].sortingOrder = 0;
 		}
 	}
 
@@ -110,5 +106,34 @@ public class GridTile : MonoBehaviour
 			_lineRenderers[i].enabled = _turnOn;
 		}
 		_turnOn = !_turnOn;
+	}
+
+	public void SetSortingOrder(int order)
+	{
+		_sortingOrder = order;
+	}
+
+	GameObject _curTileObject = null;
+	public string _spriteName { get; set; }
+	public void SetTileObjectBySprite(Sprite InSprite)
+	{
+		if (null == _curTileObject)
+		{
+			GameObject prefab = Resources.Load<GameObject>("Prefabs/TestSample2");
+			_curTileObject = Instantiate(prefab);
+			_curTileObject.GetComponent<SpriteRenderer>().sprite = InSprite;
+			_curTileObject.GetComponent<SpriteRenderer>().sortingLayerID = SortingLayer.NameToID("GROUND");
+			_curTileObject.GetComponent<SpriteRenderer>().sortingOrder = _sortingOrder;
+			Vector3 pos = transform.position;
+			pos += new Vector3(0.0f, 0.27f, 0.0f);
+			_curTileObject.transform.SetParent(transform);
+			_curTileObject.transform.position = pos;
+		}
+		else
+		{
+			_curTileObject.GetComponent<SpriteRenderer>().sprite = InSprite;
+		}
+
+		_spriteName = InSprite.name;
 	}
 }
--- Assets/01.Scripts/TileMapEditor/TileMapEditor.cs
+++ Assets/01.Scripts/TileMapEditor/TileMapEditor.cs
@@ -2,71 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+
+public enum eTileAsset
+{
+	Grass,
+	Snow,
+	MAX,
+}
 
 public class TileMapEditor : MonoBehaviour
 {
 	public GameObject _sampleTileObject;
 	public GameObject _gridPrefab;
+	public Dropdown _dropDownMenu;
+	public GameObject _dropDownContainer;
 
 	public Grid _grid;
 	public int _width;
 	public int _height;
 	public float _dragSpeed = 4.0f;
 
+	float _gridSizeX;
+	float _gridSizeY;
 	Vector3 _cameraOldPos;
 	bool _bDrag;
 
-	List<GridTile> _gridTiles = new List<GridTile>();
+	//List<GridTile> _gridTiles = new List<GridTile>();
+	List<List<GameObject>> _gridTiles = new List<List<GameObject>>();
 
 	private void Awake()
 	{
 		_bDrag = false;
+		_gridSizeX = _grid.cellSize.x;
+		_gridSizeY = _grid.cellSize.y;
 	}
 
 	// Start is called before the first frame update
 	void Start()
     {
-		for (int y = 0; y < _height; y++)
-		{
-			for (int x = 0; x < _width; x++)
-			{
-				GameObject go = Instantiate(_gridPrefab);
-				go.InitTransformAsChild(transform);
-				GridTile tile = go.GetComponent<GridTile>();
-				Vector3 pos = _grid.CellToWorld(new Vector3Int(x, y, 0));
-				tile.transform.localPosition = pos;
-				tile.MakeCollider(1.1f, 0.5f);
-				tile.MakeBoundaryLines(1.1f, 0.5f);
-
-				_gridTiles.Add(tile);
-			}
-		}
-
-		int sortingCount = _width * _height;
-		for (int y = 0; y < _height; y++)
-		{
-			for (int x = 0; x < _width; x++)
-			{
-				GameObject gameObject = Instantiate(_sampleTileObject);
-				gameObject.GetComponent<SpriteRenderer>().sortingLayerID = SortingLayer.NameToID("GROUND");
-				gameObject.GetComponent<SpriteRenderer>().sortingOrder = sortingCount--;
-				Vector3 pos = _grid.CellToLocal(new Vector3Int(x, y, 1));
-				pos += new Vector3(0.0f, 0.27f, 0.0f);
-				gameObject.transform.SetParent(transform);
-				gameObject.transform.position = pos;
-			}
-		}
+		InitDropdownMenu();
+		InitGridTiles(_width, _height);
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		if(Input.GetMouseButtonDown(1))
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(1))
 		{
 			_cameraOldPos = Camera.main.transform.position;
 		}
 
-		if(Input.GetMouseButton(1))
+		if (Input.GetMouseButton(1))
 		{
 			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 			if (Mathf.Abs(_cameraOldPos.x - pos.x) > 0 ||
@@ -89,20 +78,236 @@ public class TileMapEditor : MonoBehaviour
 		{
 			Camera.main.orthographicSize += -wheelInput;
 		}
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (null != _selectedObject)
+			{
+				Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+				if (null != hit.collider)
+				{
+					GridTile tile = hit.collider.GetComponent<GridTile>();
+
+					Sprite sprite = _selectedObject.GetComponent<Image>().sprite;
+					tile.SetTileObjectBySprite(sprite);
+				}
+			}
+		}
 	}
 
+	void InitGridTiles(int width, int height)
+	{
+		int sortingCount = width * height;
+
+		for (int y = 0; y < height; y++)
+		{
+			List<GameObject> tempList = new List<GameObject>();
+			for (int x = 0; x < width; x++)
+			{
+				GameObject go = Instantiate(_gridPrefab);
+				go.InitTransformAsChild(transform);
+				go.name = "GridTile(" + x + ", " + y + ")";
+
+				GridTile tile = go.GetComponent<GridTile>();
+				Vector3 pos = _grid.CellToWorld(new Vector3Int(x, y, 0));
+				tile.transform.localPosition = pos;
+				tile.MakeCollider(_gridSizeX, _gridSizeY);
+				tile.MakeBoundaryLines(_gridSizeX, _gridSizeY);
+				tile.SetSortingOrder(sortingCount--);
+
+				tempList.Add(go);
+			}
+			_gridTiles.Add(tempList);
+		}
+	}
+
+	void ResetGridTiles()
+	{
+		for(int i = 0; i < _gridTiles.Count; i++)
+		{
+			List<GameObject> list = _gridTiles[i];
+			for(int j = 0; j < list.Count; j++)
+			{
+				Destroy(list[j]);
+			}
+		}
+		_gridTiles.Clear();
+	}
+
+	Dictionary<eTileAsset, List<GameObject>> _dropDownList = new Dictionary<eTileAsset, List<GameObject>>();
+	Dictionary<string, Sprite> _spriteMap = new Dictionary<string, Sprite>();
+	void InitDropdownMenu()
+	{
+		for(int i = 0; i < (int)eTileAsset.MAX; i++)
+		{
+			Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites/" + ((eTileAsset)i).ToString());
+			for(int j = 0; j < sprites.Length; j++)
+				_spriteMap[sprites[j].name] = sprites[j];
+
+			List<GameObject> dropDownElements = new List<GameObject>();
+
+			for (int index = 0; index < sprites.Length; index++)
+			{
+				GameObject go = Resources.Load<GameObject>("Prefabs/UI/ListElementBtn");
+				GameObject btnPrefab = Instantiate(go);
+				btnPrefab.transform.SetParent(_dropDownContainer.transform);
+				btnPrefab.name = sprites[index].name;
+
+				Image img = btnPrefab.GetComponent<Image>();
+				img.sprite = sprites[index];
+
+				Button btn = btnPrefab.GetComponent<Button>();
+				btn.onClick.AddListener(delegate {
+					OnButtonClick(btnPrefab);
+				});
+
+				dropDownElements.Add(btnPrefab);
+			}
+
+			_dropDownList.Add((eTileAsset)i, dropDownElements);
+		}
+
+		_dropDownMenu.onValueChanged.AddListener(delegate {
+			DropdownValueChanged(_dropDownMenu);
+		});
+
+		_dropDownMenu.onValueChanged.Invoke(0);
+	}
+
+	#region UI EVENT
 	public void OpenFile()
 	{
-		string path = EditorUtility.OpenFilePanel("OpenFile", "", "*");
-		if (path.Length != 0)
-			Debug.Log(path);
+		string path = EditorUtility.OpenFilePanel("OpenFile", "", "csv");
+		if (0 != path.Length)
+		{
+			ResetGridTiles();
+			StreamReader sr = new StreamReader(path);
+			{
+				//parsing map size
+				string records = sr.ReadLine();
+				string[] tokens = records.Split(',');
+				int width = int.Parse(tokens[1]);
+				int height = int.Parse(tokens[2]);
+				InitGridTiles(width, height);
+			}
+			{
+				//parsing map data
+				sr.ReadLine();			//skip first line
+				int row = 0;
+				while (!sr.EndOfStream)
+				{
+					string records = sr.ReadLine();
+					string[] tokens = records.Split(',');
+					for(int i = 0; i < tokens.Length; i++)
+					{
+						GridTile tile = _gridTiles[row][i].GetComponent<GridTile>();
+						string spriteName = tokens[i];
+						if (spriteName.Equals("none"))
+							continue;
+						Sprite sprite = _spriteMap[spriteName];
+						tile.SetTileObjectBySprite(sprite);
+					}
+					row++;
+				}
+			}
+		}
+	}
+
+	public void SaveFile()
+	{
+		string path = EditorUtility.SaveFilePanel("SaveFile", "", "", "csv");
+		if(0 != path.Length)
+		{
+			List<List<string>> rowData = new List<List<string>>();
+			{
+				//ex)mapSize width, height
+				List<string> rowTemp = new List<string>();
+				rowTemp.Add("mapSize");
+				rowTemp.Add(_width.ToString());
+				rowTemp.Add(_height.ToString());
+				rowData.Add(rowTemp);
+			}
+			{
+				//ex)mapData
+				List<string> rowTemp = new List<string>();
+				rowTemp.Add("mapData");
+				rowData.Add(rowTemp);
+			}
+			{
+				for(int y = 0; y < _height; y++)
+				{
+					List<string> rowTemp = new List<string>();
+					for (int x = 0; x < _width; x++)
+					{
+						string tileName = _gridTiles[y][x].GetComponent<GridTile>()._spriteName;
+						rowTemp.Add(tileName);
+					}
+					rowData.Add(rowTemp);
+				}
+			}
+			{
+				//output with delimeter ","
+				StringBuilder sb = new StringBuilder();
+				for (int i = 0; i < rowData.Count; i++)
+				{
+					for(int j = 0; j < rowData[i].Count; j++)
+					{
+						if(j != rowData[i].Count - 1)
+						{
+							sb.Append(rowData[i][j] + ",");
+						}
+						else
+						{
+							sb.AppendLine(rowData[i][j]);
+						}
+					}
+				}
+				
+				StreamWriter outStream = File.CreateText(path);
+				outStream.Write(sb);
+				outStream.Close();
+
+				Debug.Log("Save completed: " + path);
+			}
+		}
 	}
 
 	public void TurnOnOffGrid()
 	{
-		for(int i = 0; i < _gridTiles.Count; i++)
+		for(int y = 0; y < _height; y++)
 		{
-			_gridTiles[i].TurnOnOff();
+			for(int x = 0; x < _width; x++)
+			{
+				_gridTiles[y][x].GetComponent<GridTile>().TurnOnOff();
+			}
 		}
 	}
+
+	void DropdownValueChanged(Dropdown change)
+	{
+		eTileAsset selected = (eTileAsset)System.Enum.Parse(typeof(eTileAsset), _dropDownMenu.options[change.value].text);
+		for (int i = 0; i < (int)eTileAsset.MAX; i++)
+		{
+			eTileAsset tileGroup = (eTileAsset)i;
+			List<GameObject> list = _dropDownList[tileGroup];
+			if(tileGroup == selected)
+			{
+				for(int j = 0; j < list.Count; j++)
+					list[j].SetActive(true);
+			}
+			else
+			{
+				for (int j = 0; j < list.Count; j++)
+					list[j].SetActive(false);
+			}
+		}
+	}
+
+	GameObject _selectedObject = null;
+	void OnButtonClick(GameObject btn)
+	{
+		_selectedObject = btn;
+	}
+	#endregion
 }
